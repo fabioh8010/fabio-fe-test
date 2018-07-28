e2a6fb6e68de5e12d1234e4c5ccdab411d4aadf2
Fábio Henriques <fabio.henriques@oneforce.com.br>
2018-07-28 18:10:35 -0300

    Setup inicial do projeto

---
+++ README.md
@@ -0,0 +1,25 @@
+# FABIO-FE-TEST
+
+## How to build the project
+
+### 1. Install project dependencies
+
+```bash
+npm install
+```
+
+### 2. Install Gulp if you don't have it yet
+
+```bash
+npm install -g gulp
+```
+
+### 3. Run build task
+
+```bash
+gulp
+```
+
+## How to run the project
+
+Go to `dist/` folder and open `index.html` file.
---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+const gulp = require('gulp')
+const standard = require('gulp-standard')
+const clean = require('gulp-clean')
+const uglify = require('gulp-uglify')
+const htmlmin = require('gulp-htmlmin')
+const cleanCSS = require('gulp-clean-css')
+const runSequence = require('run-sequence')
+const browserSync = require('browser-sync').create()
+const sass = require('gulp-sass')
+
+gulp.task('default', ['build'])
+
+// Build tasks
+
+gulp.task('build', function (callback) {
+  runSequence('clean-dist-folder', ['hint-js', 'uglify-js', 'minify-html', 'minify-css'], callback)
+})
+
+gulp.task('clean-dist-folder', function () {
+  return gulp.src('dist')
+    .pipe(clean())
+})
+
+gulp.task('hint-js', function () {
+  return gulp.src('src/scripts/scripts.js')
+    .pipe(standard())
+    .pipe(standard.reporter('default', {
+      breakOnError: true,
+      quiet: true
+    }))
+})
+
+gulp.task('uglify-js', function () {
+  return gulp.src('src/scripts/scripts.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts'))
+})
+
+gulp.task('minify-html', function () {
+  return gulp.src('src/index.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('minify-css', function () {
+  return gulp.src('src/styles/styles.scss')
+    .pipe(sass())
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('dist/styles'))
+})
+
+
+// Development tasks
+
+gulp.task('serve', ['sass-serve'], function () {
+  browserSync.init({
+    server: 'src/'
+  })
+
+  gulp.watch('src/styles/styles.scss', ['sass-serve']);
+  gulp.watch('src/scripts/scripts.js').on('change', browserSync.reload);
+  gulp.watch('src/index.html').on('change', browserSync.reload);
+})
+
+gulp.task('sass-serve', function () {
+  return gulp.src('src/styles/styles.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('src/styles'))
+    .pipe(browserSync.stream())
+})
