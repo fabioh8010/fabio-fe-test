8a9e514ee8334ad01f1277330b397811a787a369
Fábio Henriques <fabio.henriques@oneforce.com.br>
2018-07-30 21:30:35 -0300

    Implementa scroll e menu sticky

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,2 +1,16 @@
-var header = document.getElementById('header')
-header.innerHTML = 'HEADER'
+function smoothScroll (id) { // eslint-disable-line no-unused-vars
+  var element = document.getElementById(id)
+  window.scroll({ top: element.offsetTop - 75, left: 0, behavior: 'smooth' })
+}
+
+window.onscroll = function () { handleStickyNav() }
+var nav = document.getElementById('nav')
+var sticky = nav.offsetTop
+
+function handleStickyNav () {
+  if (window.pageYOffset >= sticky) {
+    nav.classList.add('sticky-nav')
+  } else {
+    nav.classList.remove('sticky-nav')
+  }
+}
